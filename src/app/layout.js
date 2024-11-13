d9405f0bad6ac3222fb0a3d095345cc846f0cefe
Lucja Bialobrzeska <lucja.bialobrzeska@gmail.com>
2024-11-13 20:05:57 +0100

    tag update

--- src/app/layout.js
+++ src/app/layout.js
@@ -30,7 +30,7 @@ const icons = {
 };
 
 export const metadata = {
-    title: "Adelina Interiors | Projektant wnętrz | Aranżacja | Gdańsk, Trójmiasto",
+    title: "Adelina Interiors | Projektant wnętrz | Aranżacja | Gdańsk, Trójmieście",
     description: "Profesjonalne projektowanie wnętrz w Gdańsku i Trójmieście. Nowoczesne, stylowe aranżacje mieszkań, domów i przestrzeni komercyjnych.",
     keywords: "projektant wnętrz Gdańsk, architekt wnętrz Gdańsk, aranżacja wnętrz Gdańsk, projektowanie wnętrz Gdańsk, projektant wnętrz Trójmiasto, architekt wnętrz Trójmiasto, aranżacja wnętrz Trójmiasto, projektowanie wnętrz Trójmiasto",
     openGraph: {
@@ -81,24 +81,20 @@ export default function RootLayout({ children }) {
             <meta property="og:image:width" content={metadata.openGraph.images[0].width} />
             <meta property="og:image:height" content={metadata.openGraph.images[0].height} />
             <meta property="og:image:alt" content={metadata.openGraph.images[0].alt} />
+
+            {/* Google Tag (gtag.js) */}
+            <Script src="https://www.googletagmanager.com/gtag/js?id=AW-16746824255" strategy="afterInteractive" />
+            <Script id="gtag-init" strategy="afterInteractive">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'AW-16746824255');
+                `}
+            </Script>
         </Head>
 
-        {/* Google Tag Manager - Script (runs before content loads) */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
-            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-WXMB26MM');`}
-        </Script>
-
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {/* Google Tag Manager (noscript) - placed directly in <body> */}
-        <noscript>
-            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WXMB26MM"
-                    height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe>
-        </noscript>
-
         {children}
         </body>
         </html>
