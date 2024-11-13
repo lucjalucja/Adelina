9ba6a6c7e37211e65285a6e2be14065ae8a292a5
Lucja Bialobrzeska <lucja.bialobrzeska@gmail.com>
2024-11-13 17:32:06 +0100

    preloading images

--- src/app/page.js
+++ src/app/page.js
@@ -66,6 +66,14 @@ export default function Home() {
         };
     }, [isModalOpen]);
 
+    useEffect(() => {
+        if (activeProject) {
+            activeProject.images.forEach((imageSrc) => {
+                const img = new Image();
+                img.src = imageSrc;
+            });
+        }
+    }, [activeProject]);
 
     const openModal = (project) => {
         setActiveProject(project);
